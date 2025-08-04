813ec91dd72af6f1f4a5939ae693160879bc5612
nickster716 <nikhil716716@gmail.com>
2025-08-04 15:42:26 +0530

    first commit

---
+++ app/Shared/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvOHx-97mCrN5xrH6wrhti9I1TdVCth50",
+  authDomain: "pinterest-clone-fc3c4.firebaseapp.com",
+  projectId: "pinterest-clone-fc3c4",
+  storageBucket: "pinterest-clone-fc3c4.firebasestorage.app",
+  messagingSenderId: "684719630583",
+  appId: "1:684719630583:web:689f81ed2dc7c662938bf2",
+  measurementId: "G-WN15Y747C9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+      "platform-lookaside.fbsbx.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
